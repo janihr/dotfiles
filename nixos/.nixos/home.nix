c3330b095147b9993481c2421fcf682abe7b2fb7
Jannis Ihrig <jannis.ihrig@obstkiste.org>
2026-03-09 14:27:21 +0100

    add nixos configs

---
+++ nixos/.nixos/home.nix
@@ -0,0 +1,15 @@
+{ config, pkgs, ... }:
+
+{
+  
+  home.username = "jarig";
+  home.homeDirectory = "/home/jarig";
+  
+  # Packages that should be installed to the user profile.
+  home.packages = with pkgs; [
+    stow
+    bitwarden-desktop
+  ];
+
+  home.stateVersion = "25.11";
+}
