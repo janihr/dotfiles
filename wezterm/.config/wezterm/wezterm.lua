7e3e8d75bd5bc551d72e0350c6ac3fc319aa6dc6
Jannis Ihrig <jannis.ihrig@aisec.fraunhofer.de>
2026-07-02 21:23:36 +0200

    wezterm: enable enhanced keyboard protocol

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -4,4 +4,7 @@ local config = wezterm.config_builder()
 -- Disable the close confirmation dialog completely
 config.window_close_confirmation = 'NeverPrompt'
 
+-- advanced keyboard protocol
+config.enable_kitty_keyboard = true
+
 return config
